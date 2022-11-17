5d691eb8d663c18fd390245dcc56ee81d477fef9
Max <memax@live.de>
2022-11-17 21:13:41 +0100

    Added Stadtwerke Aschaffenburg source

---
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/stadtwerke_aschaffenburg_de.py
@@ -0,0 +1,45 @@
+import json
+import logging
+
+import requests
+from waste_collection_schedule import Collection  # type: ignore[attr-defined]
+from waste_collection_schedule.service.ICS import ICS
+
+_LOGGER = logging.getLogger(__name__)
+
+TITLE = "Stadtwerke Aschaffenburg"
+DESCRIPTION = "Source for Stadtwerke Aschaffenburg."
+URL = "https://www.stwab.de/"
+TEST_CASES = {"Bezirk": {"bezirk": "1649", "year": 2022}}
+
+
+class Source:
+    def __init__(self, bezirk, year):
+        self._bezirk_id = bezirk
+        self._year = year
+        self._ics = ICS()
+
+    def fetch(self):
+        params = {
+            "Bezirk_ID": self._bezirk_id,
+            "Jahr": self._year,
+            "SessionMandant": "Aschaffenburg",
+            "Anwendung": "Abfuhrkalender",
+            "Methode": "TermineAnzeigenICS",
+            "Mandant": "Aschaffenburg",
+            "Abfuhrkalender": "Aschaffenburg",
+        }
+
+        # download ICS File (Fill User-Agent to prevent 403)
+        r = requests.get(
+            "https://www.stwab.de/aschaffenburgGips/Gips",
+            params=params,
+            headers={"User-Agent": "Mozilla/5.0"},
+        )
+
+        dates = self._ics.convert(r.text)
+
+        entries = []
+        for d in dates:
+            entries.append(Collection(d[0], d[1]))
+        return entries
